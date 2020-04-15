0e36a22b21720ff7f511a3be4dfefbdb8291e5c4
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2020-04-15 17:10:58 +0500

    Добавлены скрипты к кнопкам

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function replaceButton() {
+    $('#form-zakaz1').replaceWith('<input type="submit" class="button form__button1" value="Заказ оформлен" id="form-zakaz2">');
+    }
+
+function replaceButton1() {
+    $('#discount1').replaceWith('<input type="submit" class="button form__button1" value="Заказ оформлен" id="discount2">');
+    }
+
+function replaceButton2() {
+    $('#faq-form1').replaceWith('<input type="submit" class="button form__button1" value="Отправлено" id="faq-form2">');
+    }
